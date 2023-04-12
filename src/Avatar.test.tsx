65ce25888b327dd5c6521a3b647a014262ea26fa
Chan <channyein.k@gmail.com>
2023-04-12 15:00:15 +0700

    implemented test cases for onError

--- src/Avatar.test.tsx
+++ src/Avatar.test.tsx
@@ -1,5 +1,5 @@
-import { describe, expect, it } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 
 import Avatar, {
   FALLBACK_AVATAR_ALT_TEXT,
@@ -7,8 +7,11 @@ import Avatar, {
 } from './Avatar';
 
 describe('Avatar', () => {
+  beforeEach(() => {
+    cleanup();
+  });
   it('should render an img tag', () => {
-    const url = 'https://avatars.githubusercontent.com/u/11247099?v=4';
+    const url = 'https://avatars.githubusercontent.com/u/1883622?v=4';
     const alt = '@github-handle';
     render(<Avatar url={url} alt={alt} />);
 
@@ -18,7 +21,7 @@ describe('Avatar', () => {
   });
 
   it('should render the url and alt passed in', () => {
-    const url = 'https://avatars.githubusercontent.com/u/11247099?v=4';
+    const url = 'https://avatars.githubusercontent.com/u/1883622?v=4';
     const alt = 'avatar image';
     render(<Avatar url={url} alt={alt} />);
     const img = screen.getByAltText(alt);
@@ -28,6 +31,22 @@ describe('Avatar', () => {
   it('should use a fallback image if no URL passed in', () => {
     render(<Avatar />);
     const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT);
+    expect(img).toHaveAttribute('src', FALLBACK_AVATAR_URL);
+  });
+
+  it('should use fallback image if image fails to load', async () => {
+    render(<Avatar url="https://exampleimage.com" />);
+
+    const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT);
+    fireEvent.error(img);
+    expect(img).toHaveAttribute('src', FALLBACK_AVATAR_URL);
+  });
+
+  it('should use a fallback image if urll is empty string', async () => {
+    render(<Avatar url="" />);
+
+    const img = screen.getByAltText(FALLBACK_AVATAR_ALT_TEXT);
+    fireEvent.error(img);
     expect(img).toHaveAttribute('src', FALLBACK_AVATAR_URL);
   });
 });
--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
+
 type AvatarProps = {
   url?: string;
   alt?: string;
 };
 
 export const FALLBACK_AVATAR_URL =
-  'https://avatars.githubusercontent.com/u/11247099?v=4';
+  'https://avatars.githubusercontent.com/u/1883622?v=4';
 export const FALLBACK_AVATAR_ALT_TEXT = 'Avatar image';
 
 const Avatar = ({
   url = FALLBACK_AVATAR_URL,
   alt = FALLBACK_AVATAR_ALT_TEXT,
 }: AvatarProps) => {
-  return <img src={url} alt={alt} />;
+  const [srcToRender, setSrcToRender] = useState(url);
+
+  return (
+    <img
+      src={srcToRender}
+      alt={alt}
+      onError={() => setSrcToRender(FALLBACK_AVATAR_URL)}
+    />
+  );
 };
 
 export default Avatar;
