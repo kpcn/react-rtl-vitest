fd6efe1c04448cd7dd501815649c87f1da0d8999
Chan <channyein.k@gmail.com>
2023-04-12 15:21:54 +0700

    udpated heading test case for app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,6 +6,6 @@ import App from './App';
 describe('App', () => {
   it('should render with the title visible', () => {
     render(<App />);
-    expect(screen.getByText(/Vite \+ React/i)).toBeInTheDocument();
+    expect(screen.getByText(/React RTL Vitest/i)).toBeInTheDocument();
   });
 });
